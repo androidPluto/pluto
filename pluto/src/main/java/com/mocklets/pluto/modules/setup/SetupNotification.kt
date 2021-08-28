7f78692e9037765ba7732abe9a7a23f2845b685c
Prateek Srivastava <srtv.prateek@gmail.com>
2021-08-29 00:28:50 +0530

    mutability flag added to notification pending indents (#47)

--- pluto/src/main/java/com/mocklets/pluto/modules/setup/SetupNotification.kt
+++ pluto/src/main/java/com/mocklets/pluto/modules/setup/SetupNotification.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import com.mocklets.pluto.R
 import com.mocklets.pluto.core.notification.NotificationUtil
 import com.mocklets.pluto.ui.PlutoActivity
@@ -18,7 +19,11 @@ internal class SetupNotification(private val context: Context) {
 
     fun add() {
         val notificationIntent = Intent(context, PlutoActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0)
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(context, 0, notificationIntent, 0)
+        }
 
         notificationUtil.notify(
             title = context.getString(R.string.pluto___notification_title, clientAppName),
