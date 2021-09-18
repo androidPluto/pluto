8bcf496f416025893ae0c6167982706ab2fe8d8f
srtvprateek <srtv.prateek@gmail.com>
2021-09-18 23:58:07 +0530

    ktlint fix

--- sample/src/main/java/com/sampleapp/SecondActivity.kt
+++ sample/src/main/java/com/sampleapp/SecondActivity.kt
@@ -1,7 +1,7 @@
 package com.sampleapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
