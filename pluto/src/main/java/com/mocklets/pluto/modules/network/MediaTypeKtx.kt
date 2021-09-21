792527ef5e44a29704a9b4ad12282f761b24b831
srtvprateek <srtv.prateek@gmail.com>
2021-09-21 17:35:23 +0530

    handle *json media subtype

--- pluto/src/main/java/com/mocklets/pluto/modules/network/MediaTypeKtx.kt
+++ pluto/src/main/java/com/mocklets/pluto/modules/network/MediaTypeKtx.kt
@@ -7,14 +7,14 @@ import okhttp3.MediaType
 
 internal fun MediaType.isText(): Boolean {
     return (type() == "application" || type() == "text") &&
-        (subtype() == "json" || subtype() == "plain" || subtype() == "xml" || subtype() == "html" || subtype() == "x-www-form-urlencoded")
+        (subtype().endsWith("json") || subtype() == "plain" || subtype() == "xml" || subtype() == "html" || subtype() == "x-www-form-urlencoded")
 }
 
 internal fun MediaType.beautify(plain: CharSequence, indent: Int = BODY_INDENTATION): CharSequence? {
-    return when (subtype()) {
-        "json" -> JsonBaseTransformer().beautify(plain, indent) // beautifyJson(plain, indent)
-        "xml", "html" -> XmlBaseTransformer().beautify(plain, indent)
-        "x-www-form-urlencoded" -> FormEncodedTransformer().beautify(plain)
+    return when {
+        subtype().endsWith("json") -> JsonBaseTransformer().beautify(plain, indent)
+        subtype() == "xml" || subtype() == "html" -> XmlBaseTransformer().beautify(plain, indent)
+        subtype() == "x-www-form-urlencoded" -> FormEncodedTransformer().beautify(plain)
         else -> plain
     }
 }
