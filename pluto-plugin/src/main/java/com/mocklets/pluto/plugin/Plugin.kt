c1154cb84af99e32c641869a86681cd6921244d6
srtvprateek <srtv.prateek@gmail.com>
2021-11-07 00:43:57 +0530

    init

---
+++ pluto-plugin/src/main/java/com/mocklets/pluto/plugin/Plugin.kt
@@ -0,0 +1,36 @@
+package com.mocklets.pluto.plugin
+
+import android.content.Context
+
+abstract class Plugin {
+
+    val context: Context
+        get() = returnContext()
+
+    fun install(context: Context) {
+        this._context = context
+        onPluginInstalled()
+    }
+
+    private var _context: Context? = null
+    private fun returnContext(): Context {
+        _context?.let {
+            return@let it
+        }
+        throw IllegalStateException("${this.javaClass.name} plugin is not installed yet.")
+    }
+
+    abstract fun getConfig(): PluginConfiguration
+
+    abstract fun onPluginInstalled()
+
+    open fun shouldInstallPlugin(): Boolean = true
+
+    override fun equals(other: Any?): Boolean {
+        return other is Plugin && getConfig().hashCode() == other.getConfig().hashCode()
+    }
+
+    override fun hashCode(): Int {
+        return getConfig().hashCode()
+    }
+}
--- sample/src/main/java/com/sampleapp/SampleApp.kt
+++ sample/src/main/java/com/sampleapp/SampleApp.kt
@@ -2,24 +2,38 @@ package com.sampleapp
 
 import android.app.Application
 import android.util.Log
-import com.mocklets.pluto.Pluto
-import com.mocklets.pluto.PlutoLog
-import com.mocklets.pluto.modules.exceptions.ANRException
-import com.mocklets.pluto.modules.exceptions.ANRListener
+import com.mocklets.pluto.module.Pluto
+import com.mocklets.pluto.plugin.Plugin
+import com.mocklets.pluto.plugin.PluginConfiguration
 
 class SampleApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Pluto.initialize(this)
-        Pluto.setANRListener(object : ANRListener {
-            override fun onAppNotResponding(exception: ANRException) {
-                exception.printStackTrace()
-                PlutoLog.e("anr-exception", exception.threadStateMap)
+
+        val samplePlugin = object : Plugin() {
+            override fun getConfig(): PluginConfiguration {
+                return PluginConfiguration(
+                    name = "sample module _Aosdfm"
+                )
             }
-        })
-        Pluto.setExceptionHandler { thread, tr ->
-            Log.d("exception", "uncaught exception handled on thread: " + thread.name, tr)
+
+            override fun onPluginInstalled() {
+                Log.d("Prateek", getConfig().identifier)
+            }
         }
+        Pluto.Installer(this)
+            .addPlugin(samplePlugin)
+            .install()
+//        Pluto.initialize(this)
+//        Pluto.setANRListener(object : ANRListener {
+//            override fun onAppNotResponding(exception: ANRException) {
+//                exception.printStackTrace()
+//                PlutoLog.e("anr-exception", exception.threadStateMap)
+//            }
+//        })
+//        Pluto.setExceptionHandler { thread, tr ->
+//            Log.d("exception", "uncaught exception handled on thread: " + thread.name, tr)
+//        }
     }
 }
