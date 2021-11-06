3e60f83b6b6b56aab7542e6f3bc9dbb00aaa773a
srtvprateek <srtv.prateek@gmail.com>
2021-11-07 00:44:16 +0530

    init

--- pluto-plugin/src/main/java/com/mocklets/pluto/plugin/PluginConfiguration.kt
+++ pluto-plugin/src/main/java/com/mocklets/pluto/plugin/PluginConfiguration.kt
@@ -1,13 +1,12 @@
 package com.mocklets.pluto.plugin
 
 import androidx.annotation.DrawableRes
-import java.util.*
 
 data class PluginConfiguration(
     val name: String,
     @DrawableRes val icon: Int = R.drawable.pluto_plugin___ic_plugin_placeholder_icon,
 ) {
-    val identifier = name.lowercase(Locale.getDefault()).replace(" ", "_", true)
+    val identifier = name.lowercase().replace(" ", "_", true)
 
     override fun equals(other: Any?): Boolean {
         return other is PluginConfiguration && identifier == other.identifier
