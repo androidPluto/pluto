e7cb6c47bb747223a5d07bb9787ec457977b7630
srtvprateek <srtv.prateek@gmail.com>
2021-11-27 17:54:18 +0530

    refactor

--- pluto-core/src/main/java/com/mocklets/pluto/ui/PlutoActivity.kt
+++ pluto-core/src/main/java/com/mocklets/pluto/ui/PlutoActivity.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.mocklets.pluto.databinding.PlutoActivityPlutoBinding
 
 class PlutoActivity : AppCompatActivity() {
+    private lateinit var drawerManager: NavigationDrawerManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = PlutoActivityPlutoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val drawerManager = NavigationDrawerManager(this, binding) {
+        drawerManager = NavigationDrawerManager(this, binding) {
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
         drawerManager.init()
     }
 }
