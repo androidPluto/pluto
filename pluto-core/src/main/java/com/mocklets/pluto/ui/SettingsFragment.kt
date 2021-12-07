03c3cfc72e528a7940c0dded483787300c70f084
srtvprateek <srtv.prateek@gmail.com>
2021-12-07 10:46:55 +0530

    settings ui

--- pluto-core/src/main/java/com/mocklets/pluto/ui/SettingsFragment.kt
+++ pluto-core/src/main/java/com/mocklets/pluto/ui/SettingsFragment.kt
@@ -5,18 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mocklets.pluto.R
-import com.mocklets.pluto.databinding.PlutoFragmentBaseBinding
+import com.mocklets.pluto.databinding.PlutoFragmentSettingsBinding
 import com.mocklets.pluto.utilities.setDebounceClickListener
 import com.mocklets.pluto.utilities.viewBinding
 
-class BaseFragment : Fragment(R.layout.pluto___fragment_base) {
+class SettingsFragment : Fragment(R.layout.pluto___fragment_settings) {
 
-    private val binding by viewBinding(PlutoFragmentBaseBinding::bind)
+    private val binding by viewBinding(PlutoFragmentSettingsBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.settings.setDebounceClickListener {
-            findNavController().navigate(R.id.openSettings)
+        binding.back.setDebounceClickListener {
+            findNavController().popBackStack()
         }
     }
 }
