f5d6616721e34071b66346d46d58dee9bdf9bc9d
srtvprateek <srtv.prateek@gmail.com>
2021-12-20 01:29:22 +0530

    logs detail setup

--- plugin-logger/src/main/java/com/mocklets/pluto/logger/ui/DetailsFragment.kt
+++ plugin-logger/src/main/java/com/mocklets/pluto/logger/ui/DetailsFragment.kt
@@ -1,12 +1,14 @@
 package com.mocklets.pluto.logger.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mocklets.pluto.logger.LogData
 import com.mocklets.pluto.logger.LogsViewModel
 import com.mocklets.pluto.logger.R
@@ -15,10 +17,15 @@ import com.mocklets.pluto.utilities.extensions.toast
 import com.mocklets.pluto.utilities.setDebounceClickListener
 import com.mocklets.pluto.utilities.viewBinding
 
-internal class DetailsFragment : Fragment(R.layout.pluto_logger___fragment_details) {
+internal class DetailsFragment : BottomSheetDialogFragment() {
 
     private val binding by viewBinding(PlutoLoggerFragmentDetailsBinding::bind)
     private val viewModel: LogsViewModel by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.pluto_logger___fragment_details, container, false)
+
+    override fun getTheme(): Int = R.style.PlutoLoggerBottomSheetDialog
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
