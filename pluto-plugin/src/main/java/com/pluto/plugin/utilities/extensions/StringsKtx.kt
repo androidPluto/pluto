65f618c8197846ae5f88670ff270ad1138fd2635
srtvprateek <srtv.prateek@gmail.com>
2022-01-15 23:57:04 +0530

    pluto-plugin cleanup

--- pluto-plugin/src/main/java/com/pluto/plugin/utilities/extensions/StringsKtx.kt
+++
@@ -1,6 +0,0 @@
-package com.pluto.plugin.utilities.extensions
-
-import java.util.Locale
-
-fun String.capitalizeText(default: Locale = Locale.getDefault()): String =
-    replaceFirstChar { if (it.isLowerCase()) it.titlecase(default) else it.toString() }
