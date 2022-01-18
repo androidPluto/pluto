40d3bb75e745bc32ee484e5a7594bc00638bb059
Jimly Asshiddiqy <j_mly@ymail.com>
2022-01-18 12:47:02 +0700

    refactor:  - separate code based on build variant  - remove buildToolsVersion warning

---
+++ pluto/src/debug/java/com/mocklets/pluto/modules/network/NetworkCallsRepo.kt
@@ -0,0 +1,42 @@
+package com.mocklets.pluto.modules.network
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.*
+import kotlin.collections.LinkedHashMap
+import kotlin.collections.set
+
+internal object NetworkCallsRepo {
+
+    val apiCalls: LiveData<List<ApiCallData>>
+        get() = _apiCalls
+    private val _apiCalls = MutableLiveData<List<ApiCallData>>()
+    private val apiCallMap = Collections.synchronizedMap(LinkedHashMap<String, ApiCallData>())
+
+    fun set(request: ApiCallData) {
+        synchronized(apiCallMap) {
+            apiCallMap[request.id] = request
+            apiCallMap.updateLiveData()
+        }
+    }
+
+    fun get(id: String): ApiCallData? {
+        return synchronized(apiCallMap) {
+            apiCallMap.getOrElse(id) { null }
+        }
+    }
+
+    fun deleteAll() {
+        synchronized(apiCallMap) {
+            apiCallMap.clear()
+            apiCallMap.updateLiveData()
+        }
+    }
+
+    private fun MutableMap<String, ApiCallData>.updateLiveData() {
+        val list = arrayListOf<ApiCallData>()
+        list.addAll(values)
+        list.reverse()
+        _apiCalls.postValue(list)
+    }
+}
---
+++ pluto/src/debug/java/com/mocklets/pluto/modules/settings/SettingsAdapter.kt
@@ -0,0 +1,42 @@
+package com.mocklets.pluto.modules.settings
+
+import android.view.ViewGroup
+import com.mocklets.pluto.core.ui.list.BaseAdapter
+import com.mocklets.pluto.core.ui.list.DiffAwareHolder
+import com.mocklets.pluto.core.ui.list.ListItem
+import com.mocklets.pluto.modules.settings.holders.*
+
+internal class SettingsAdapter(private val listener: OnActionListener) : BaseAdapter() {
+    override fun getItemViewType(item: ListItem): Int? {
+        return when (item) {
+            is SettingsEasyAccessEntity -> ITEM_TYPE_EASY_ACCESS
+            is SettingsClearDataEntity -> ITEM_TYPE_CLEAR_CRASHES
+            is SettingsSharedPrefEntity -> ITEM_TYPE_SHARED_PREF
+            is SettingsLinkMockletsEntity -> ITEM_TYPE_MOCKLETS
+            is SettingsEasyAccessPopupAppearanceEntity -> ITEM_TYPE_EASY_ACCESS_APPEARANCE
+            is SettingsResetAllEntity -> ITEM_TYPE_RESET_ALL
+            else -> null
+        }
+    }
+
+    override fun onViewHolderCreated(parent: ViewGroup, viewType: Int): DiffAwareHolder? {
+        return when (viewType) {
+            ITEM_TYPE_EASY_ACCESS -> SettingsEasyAccessHolder(parent, listener)
+            ITEM_TYPE_CLEAR_CRASHES -> SettingsClearDataHolder(parent, listener)
+            ITEM_TYPE_SHARED_PREF -> SettingsSharePreferenceHolder(parent, listener)
+            ITEM_TYPE_MOCKLETS -> SettingsMockletsLinkHolder(parent, listener)
+            ITEM_TYPE_EASY_ACCESS_APPEARANCE -> SettingsEasyAccessPopupAppearanceHolder(parent, listener)
+            ITEM_TYPE_RESET_ALL -> SettingsResetAllHolder(parent, listener)
+            else -> null
+        }
+    }
+
+    companion object {
+        const val ITEM_TYPE_EASY_ACCESS = 1000
+        const val ITEM_TYPE_CLEAR_CRASHES = 1001
+        const val ITEM_TYPE_SHARED_PREF = 1002
+        const val ITEM_TYPE_MOCKLETS = 1003
+        const val ITEM_TYPE_EASY_ACCESS_APPEARANCE = 1004
+        const val ITEM_TYPE_RESET_ALL = 1005
+    }
+}
