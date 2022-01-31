a90796abdfee50ab3a5ce10b4a9c8eebb392d61c
srtvprateek <srtv.prateek@gmail.com>
2022-01-31 18:09:22 +0530

    delete redundant code

--- plugin-network/src/main/java/com/mocklets/pluto/network/PlutoNetwork.kt
+++
@@ -1,7 +0,0 @@
-package com.mocklets.pluto.network
-
-import android.content.Context
-
-internal object PlutoNetwork {
-    internal var appContext: Context? = null
-}
--- plugin-network/src/main/java/com/mocklets/pluto/network/internal/transformers/BaseTransformer.kt
+++
@@ -1,8 +0,0 @@
-package com.mocklets.pluto.network.internal.transformers
-
-import com.mocklets.pluto.network.internal.BODY_INDENTATION
-
-internal interface BaseTransformer {
-    fun beautify(plain: CharSequence, indent: Int = BODY_INDENTATION): CharSequence?
-    fun flatten(plain: CharSequence): String
-}
