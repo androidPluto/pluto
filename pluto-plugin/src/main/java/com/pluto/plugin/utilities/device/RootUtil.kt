95a9e476316be37018c0aada2915b203a8fea471
srtvprateek <srtv.prateek@gmail.com>
2022-02-01 20:02:01 +0530

    root util moved to class

--- pluto-plugin/src/main/java/com/pluto/plugin/utilities/device/RootUtil.kt
+++ pluto-plugin/src/main/java/com/pluto/plugin/utilities/device/RootUtil.kt
@@ -9,46 +9,48 @@ import java.io.InputStreamReader
  *
  * @author Kevin Kowalewski
  */
-internal object RootUtil {
-    val isDeviceRooted: Boolean
-        get() = checkRootMethod1() || checkRootMethod2() || checkRootMethod3()
+internal class RootUtil private constructor() {
+    companion object {
+        val isDeviceRooted: Boolean
+            get() = checkRootMethod1() || checkRootMethod2() || checkRootMethod3()
 
-    private fun checkRootMethod1(): Boolean {
-        val buildTags = Build.TAGS
-        return buildTags != null && buildTags.contains("test-keys")
-    }
-
-    private fun checkRootMethod2(): Boolean {
-        val paths = arrayOf(
-            "/system/app/Superuser.apk",
-            "/sbin/su",
-            "/system/bin/su",
-            "/system/xbin/su",
-            "/data/local/xbin/su",
-            "/data/local/bin/su",
-            "/system/sd/xbin/su",
-            "/system/bin/failsafe/su",
-            "/data/local/su",
-            "/su/bin/su"
-        )
-        for (path in paths) {
-            if (File(path).exists()) return true
+        private fun checkRootMethod1(): Boolean {
+            val buildTags = Build.TAGS
+            return buildTags != null && buildTags.contains("test-keys")
         }
-        return false
-    }
 
-    @Suppress("TooGenericExceptionCaught", "SwallowedException")
-    private fun checkRootMethod3(): Boolean {
-        var process: Process? = null
-        return try {
-            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
-            val stream = BufferedReader(InputStreamReader(process.inputStream))
-            stream.readLine() != null
-        } catch (t: Throwable) {
+        private fun checkRootMethod2(): Boolean {
+            val paths = arrayOf(
+                "/system/app/Superuser.apk",
+                "/sbin/su",
+                "/system/bin/su",
+                "/system/xbin/su",
+                "/data/local/xbin/su",
+                "/data/local/bin/su",
+                "/system/sd/xbin/su",
+                "/system/bin/failsafe/su",
+                "/data/local/su",
+                "/su/bin/su"
+            )
+            for (path in paths) {
+                if (File(path).exists()) return true
+            }
+            return false
+        }
+
+        @Suppress("TooGenericExceptionCaught", "SwallowedException")
+        private fun checkRootMethod3(): Boolean {
+            var process: Process? = null
+            return try {
+                process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
+                val stream = BufferedReader(InputStreamReader(process.inputStream))
+                stream.readLine() != null
+            } catch (t: Throwable) {
 //            DebugLog.e("root-utils", "exception occurred", t)
-            false
-        } finally {
-            process?.destroy()
+                false
+            } finally {
+                process?.destroy()
+            }
         }
     }
 }
