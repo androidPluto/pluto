d0258db722ae8c6028e71c59f0d29be2e66d90bc
srtvprateek <srtv.prateek@gmail.com>
2022-02-05 20:51:57 +0530

    removed legacy code

--- pluto/src/main/java/com/mocklets/pluto/modules/network/transformers/BaseTransformer.kt
+++
@@ -1,8 +0,0 @@
-package com.mocklets.pluto.modules.network.transformers
-
-import com.mocklets.pluto.modules.network.BODY_INDENTATION
-
-internal interface BaseTransformer {
-    fun beautify(plain: CharSequence, indent: Int = BODY_INDENTATION): CharSequence?
-    fun flatten(plain: CharSequence): String
-}
--- pluto/src/main/java/com/mocklets/pluto/modules/setup/easyaccess/OnPopupInteractionListener.kt
+++
@@ -1,8 +0,0 @@
-package com.mocklets.pluto.modules.setup.easyaccess
-
-import android.view.WindowManager
-
-internal interface OnPopupInteractionListener {
-    fun onClick()
-    fun onLayoutParamsUpdated(params: WindowManager.LayoutParams)
-}
