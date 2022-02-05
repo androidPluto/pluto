f39a162f486d6e6ad726854acbae0b0e3d74106c
srtvprateek <srtv.prateek@gmail.com>
2022-02-05 20:49:14 +0530

    code refactor

--- sample/src/main/java/com/sampleapp/SampleApp.kt
+++ sample/src/main/java/com/sampleapp/SampleApp.kt
@@ -9,37 +9,10 @@ class SampleApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-//        val samplePlugin = object : Plugin() {
-//            override fun getConfig(): PluginConfiguration {
-//                return PluginConfiguration(
-//                    name = "sample module _Aosdfm",
-//                )
-//            }
-//
-//            override fun getDeveloperDeveloperDetails(): DeveloperDetails? {
-//                return null
-//            }
-//
-//            override fun onPluginInstalled() {
-//                Log.d("Prateek", getConfig().identifier)
-//            }
-//        }
         Pluto.Installer(this)
-//            .addPlugin(PlutoNetworkPlugin())
             .addPlugin(PlutoLoggerPlugin("logger"))
             .addPlugin(PlutoSharePreferencesPlugin("sharedPref"))
             .install()
-        Pluto.showNotch(true)
-//        Pluto.initialize(this)
-//        Pluto.setANRListener(object : ANRListener {
-//            override fun onAppNotResponding(exception: ANRException) {
-//                exception.printStackTrace()
-//                PlutoLog.e("anr-exception", exception.threadStateMap)
-//            }
-//        })
-//        Pluto.setExceptionHandler { thread, tr ->
-//            Log.d("exception", "uncaught exception handled on thread: " + thread.name, tr)
-//        }
+        Pluto.showNotch(false)
     }
 }
