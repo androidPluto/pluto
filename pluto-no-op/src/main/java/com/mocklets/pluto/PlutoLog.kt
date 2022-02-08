824b973a61ded6313cffed269945bc9f71c1ec91
Prateek Srivastava <srtv.prateek@gmail.com>
2022-02-08 17:50:52 +0530

    no-op build fix (#111)

--- pluto-no-op/src/main/java/com/mocklets/pluto/PlutoLog.kt
+++ pluto-no-op/src/main/java/com/mocklets/pluto/PlutoLog.kt
@@ -3,17 +3,25 @@ package com.mocklets.pluto
 import androidx.annotation.Keep
 
 @Keep
-object PlutoLog {
+class PlutoLog private constructor() {
 
-    fun v(tag: String, message: String?, tr: Throwable? = null) {}
+    companion object {
+        @JvmStatic
+        fun v(tag: String, message: String?, tr: Throwable? = null) {}
 
-    fun d(tag: String, message: String?, tr: Throwable? = null) {}
+        @JvmStatic
+        fun d(tag: String, message: String?, tr: Throwable? = null) {}
 
-    fun i(tag: String, message: String?, tr: Throwable? = null) {}
+        @JvmStatic
+        fun i(tag: String, message: String?, tr: Throwable? = null) {}
 
-    fun w(tag: String, message: String?, tr: Throwable? = null) {}
+        @JvmStatic
+        fun w(tag: String, message: String?, tr: Throwable? = null) {}
 
-    fun e(tag: String, message: String?, tr: Throwable? = null) {}
+        @JvmStatic
+        fun e(tag: String, message: String?, tr: Throwable? = null) {}
 
-    fun event(tag: String, event: String, attributes: HashMap<String, Any?>?) {}
+        @JvmStatic
+        fun event(tag: String, event: String, attributes: HashMap<String, Any?>?) {}
+    }
 }
