6c400fe91ca59d95cbe54a8a229adc9646855700
srtvprateek <srtv.prateek@gmail.com>
2022-02-10 12:44:32 +0530

    share logic revamped (fix for memory leak)

--- pluto-plugin/src/main/java/com/pluto/plugin/utilities/sharing/ContentShareViewModel.kt
+++ pluto-plugin/src/main/java/com/pluto/plugin/utilities/sharing/ContentShareViewModel.kt
@@ -1,13 +1,24 @@
 package com.pluto.plugin.utilities.sharing
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.pluto.plugin.utilities.SingleLiveEvent
 
 class ContentShareViewModel : ViewModel() {
 
-    val data = SingleLiveEvent<Shareable>()
+    val data: LiveData<Shareable>
+        get() = _data
+    private val _data = SingleLiveEvent<Shareable>()
+
+    val action: LiveData<ShareAction>
+        get() = _action
+    private val _action = SingleLiveEvent<ShareAction>()
+
+    fun performAction(action: ShareAction) {
+        _action.postValue(action)
+    }
 
     fun share(shareable: Shareable) {
-        data.postValue(shareable)
+        _data.postValue(shareable)
     }
 }
