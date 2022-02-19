65e0c5331030b790cf32cf5b6e724703d413cc19
srtvprateek <srtv.prateek@gmail.com>
2022-02-19 14:49:51 +0530

    support for no-op added

---
+++ pluto-no-op/src/main/java/com/pluto/Pluto.kt
@@ -0,0 +1,27 @@
+package com.pluto
+
+import android.app.Application
+import android.os.Bundle
+
+@SuppressWarnings("EmptyFunctionBlock", "UnusedPrivateMember")
+object Pluto {
+
+    @JvmOverloads
+    fun open(identifier: String? = null, bundle: Bundle? = null) {
+    }
+
+    fun showNotch(state: Boolean) {
+    }
+
+    class Installer(private val application: Application) {
+
+        private val plugins = linkedSetOf<Any>()
+
+        fun addPlugin(plugin: Any): Installer {
+            return this
+        }
+
+        fun install() {
+        }
+    }
+}
