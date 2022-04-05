092c95c70d88a00775d71e56b70271c049e46ac2
srtvprateek <srtv.prateek@gmail.com>
2022-04-05 18:25:51 +0530

    base ui setup

---
+++ plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/PlutoRoomsDatabasePlugin.kt
@@ -0,0 +1,31 @@
+package com.pluto.plugins.rooms.db
+
+import androidx.fragment.app.Fragment
+import com.pluto.plugin.DeveloperDetails
+import com.pluto.plugin.Plugin
+import com.pluto.plugin.PluginConfiguration
+
+class PlutoRoomsDatabasePlugin(identifier: String) : Plugin(identifier) {
+
+    override fun getConfig(): PluginConfiguration = PluginConfiguration(
+        name = context.getString(R.string.pluto_rooms___plugin_name),
+        icon = R.drawable.pluto_rooms___ic_rooms_icon,
+        version = BuildConfig.VERSION_NAME
+    )
+
+    override fun getView(): Fragment = RoomsDBFragment()
+
+    override fun getDeveloperDetails(): DeveloperDetails {
+        return DeveloperDetails(
+            website = "https://plutolib.com",
+            vcsLink = "https://github.com/plutolib/plugin-rooms-database",
+            twitter = "https://twitter.com/pluto_lib"
+        )
+    }
+
+    override fun onPluginInstalled() {
+    }
+
+    override fun onPluginDataCleared() {
+    }
+}
