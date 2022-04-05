1c6871349ee570dc05d8e900fbc3f9f4258383c3
srtvprateek <srtv.prateek@gmail.com>
2022-04-05 23:06:19 +0530

    code refactor

--- plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/DBSelectorFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.pluto.plugins.rooms.db
-
-import androidx.fragment.app.Fragment
-
-class DBSelectorFragment : Fragment(R.layout.pluto_rooms___fragment_db_selector)
---
+++ plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/RoomsDBViewModel.kt
@@ -0,0 +1,18 @@
+package com.pluto.plugins.rooms.db.internal
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.pluto.plugins.rooms.db.PlutoRoomsDB
+
+internal class RoomsDBViewModel(application: Application) : AndroidViewModel(application) {
+
+    val dbs: LiveData<List<DatabaseModel>>
+        get() = _dbs
+    private val _dbs = MutableLiveData<List<DatabaseModel>>()
+
+    fun fetch() {
+        _dbs.postValue(PlutoRoomsDB.watchList)
+    }
+}
