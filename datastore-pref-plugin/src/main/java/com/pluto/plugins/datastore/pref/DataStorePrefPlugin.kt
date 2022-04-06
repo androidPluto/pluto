2c5bfec93519376150fa1417f67f914a43add14d
Rishabh Singh Bisht <rishabhsinghbisht@gmail.com>
2022-04-06 17:55:51 +0530

    chore: pipeline fixes

--- datastore-pref-plugin/src/main/java/com/pluto/plugins/datastore/pref/DataStorePrefPlugin.kt
+++ datastore-pref-plugin/src/main/java/com/pluto/plugins/datastore/pref/DataStorePrefPlugin.kt
@@ -1,7 +1,6 @@
 package com.pluto.plugins.datastore.pref
 
 import androidx.fragment.app.Fragment
-import com.pluto.plugins.datastore.pref.R
 import com.pluto.plugin.Plugin
 import com.pluto.plugin.PluginConfiguration
 import com.pluto.plugins.datastore.pref.internal.DatastorePreferencePluginFragment
