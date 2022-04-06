d011f903b9449a55a03247504158109474da133d
srtvprateek <srtv.prateek@gmail.com>
2022-04-07 01:03:49 +0530

    details setup

--- plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/PlutoRoomsDB.kt
+++ plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/PlutoRoomsDB.kt
@@ -1,12 +1,13 @@
 package com.pluto.plugins.rooms.db
 
+import androidx.room.RoomDatabase
 import com.pluto.plugins.rooms.db.internal.DatabaseModel
 
 object PlutoRoomsDB {
 
     internal val watchList = arrayListOf<DatabaseModel>()
 
-    fun watch(dbClass: Class<*>, dbName: String) {
+    fun watch(dbClass: Class<out RoomDatabase>, dbName: String) {
         watchList.add(DatabaseModel(dbClass, dbName))
     }
 }
