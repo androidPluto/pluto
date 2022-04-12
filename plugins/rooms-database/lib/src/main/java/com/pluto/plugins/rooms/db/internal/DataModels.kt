183052a8e50cec2062448504eefcc81bf84fac83
srtvprateek <srtv.prateek@gmail.com>
2022-04-13 01:28:33 +0530

    code refactor

--- plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/DataModels.kt
+++ plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/DataModels.kt
@@ -1,13 +1,16 @@
 package com.pluto.plugins.rooms.db.internal
 
+import androidx.annotation.Keep
 import androidx.room.RoomDatabase
 import com.pluto.plugin.utilities.list.ListItem
 
+@Keep
 internal data class DatabaseModel(
     val name: String,
     val dbClass: Class<out RoomDatabase>
 ) : ListItem()
 
+@Keep
 internal data class TableModel(
     val name: String,
     val isSystemTable: Boolean
