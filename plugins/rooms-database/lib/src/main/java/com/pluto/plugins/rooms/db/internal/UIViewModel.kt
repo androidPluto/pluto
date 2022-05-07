1a1a02d979b1df28525187daaee5434c9d6e5631
srtvprateek <srtv.prateek@gmail.com>
2022-05-07 11:40:35 +0530

    performance fixes

--- plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/UIViewModel.kt
+++ plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/UIViewModel.kt
@@ -2,23 +2,31 @@ package com.pluto.plugins.rooms.db.internal
 
 import android.app.Application
 import android.content.Context
+import android.widget.FrameLayout
 import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import com.pluto.plugin.utilities.SingleLiveEvent
+import com.pluto.plugin.utilities.extensions.forEachIndexed
+import com.pluto.plugins.rooms.db.internal.core.widgets.DataEditWidget
 import com.pluto.plugins.rooms.db.internal.core.widgets.TableGridView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 internal class UIViewModel(application: Application) : AndroidViewModel(application) {
 
-    val dataView: LiveData<HorizontalScrollView>
-        get() = _dataView
-    private val _dataView = SingleLiveEvent<HorizontalScrollView>()
+    val tableGridView: LiveData<HorizontalScrollView>
+        get() = _tableGridView
+    private val _tableGridView = SingleLiveEvent<HorizontalScrollView>()
+
+    val rowEditView: LiveData<Pair<List<DataEditWidget>, LinearLayout>>
+        get() = _rowEditView
+    private val _rowEditView = SingleLiveEvent<Pair<List<DataEditWidget>, LinearLayout>>()
 
     @SuppressWarnings("LongParameterList")
-    fun generateView(
+    fun generateTableGridView(
         context: Context,
         columns: List<ColumnModel>,
         rows: List<List<String>>,
@@ -44,7 +52,38 @@ internal class UIViewModel(application: Application) : AndroidViewModel(applicat
                     onColumnLongClick.invoke(column)
                 }
             ).also { view -> hsv.addView(view) }
-            _dataView.postValue(hsv)
+            _tableGridView.postValue(hsv)
         }
+    }
+
+    fun generateRowEditView(context: Context, dataConfig: RowDetailsData) {
+        val etList = mutableListOf<DataEditWidget>()
+        val mainLayout = LinearLayout(context).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            orientation = LinearLayout.VERTICAL
+        }
+
+        val columns = dataConfig.columns
+        val rows = dataConfig.values ?: dataConfig.columns.map { col ->
+            col.defaultValue?.replace("\'", "")
+        }
+        Pair(columns, rows).forEachIndexed { _, column, value ->
+            val valueEditText = DataEditWidget(context)
+            etList.add(valueEditText)
+            valueEditText.create(column, value)
+            val row = LinearLayout(context).apply {
+                layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+                )
+                orientation = LinearLayout.VERTICAL
+                addView(valueEditText)
+            }
+            mainLayout.addView(row)
+        }
+        _rowEditView.postValue(Pair(etList, mainLayout))
     }
 }
