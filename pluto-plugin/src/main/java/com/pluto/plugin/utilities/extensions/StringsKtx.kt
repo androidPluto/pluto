f45e83b6e8d42690d0105f63805e5a25bc1a685c
srtvprateek <srtv.prateek@gmail.com>
2022-05-10 02:47:01 +0530

    ui cleanup

--- pluto-plugin/src/main/java/com/pluto/plugin/utilities/extensions/StringsKtx.kt
+++ pluto-plugin/src/main/java/com/pluto/plugin/utilities/extensions/StringsKtx.kt
@@ -4,3 +4,11 @@ import java.util.Locale
 
 fun String.capitalizeText(default: Locale = Locale.getDefault()): String =
     replaceFirstChar { if (it.isLowerCase()) it.titlecase(default) else it.toString() }
+
+fun String.truncateExcess(offset: Int): String {
+    return if (length > offset) {
+        "${take(offset)}..."
+    } else {
+        this
+    }
+}
