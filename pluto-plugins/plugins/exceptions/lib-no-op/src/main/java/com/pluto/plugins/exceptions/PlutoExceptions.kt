3f6c8e0c22039ce06d9a890768981c5bad72b7fd
srtvprateek <srtv.prateek@gmail.com>
2023-01-18 00:30:08 +0530

    fix(exceptions): no-op library integration fix

--- pluto-plugins/plugins/exceptions/lib-no-op/src/main/java/com/pluto/plugins/exceptions/PlutoExceptions.kt
+++ pluto-plugins/plugins/exceptions/lib-no-op/src/main/java/com/pluto/plugins/exceptions/PlutoExceptions.kt
@@ -3,6 +3,12 @@ package com.pluto.plugins.exceptions
 @SuppressWarnings("UnusedPrivateMember", "EmptyFunctionBlock")
 object PlutoExceptions {
 
+    /**
+     * The threshold for main thread response
+     * time before resulting in ANR.
+     */
+    var mainThreadResponseThreshold = 0
+
     fun setExceptionHandler(uncaughtExceptionHandler: Thread.UncaughtExceptionHandler) {
     }
 
