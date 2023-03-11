72af8578688009546c996b10b1fa3917769a42f9
srtvprateek <srtv.prateek@gmail.com>
2023-03-12 01:00:13 +0530

    key value pair editor init

--- pluto-plugins/base/lib/src/main/java/com/pluto/utilities/views/keyvalue/KeyValuePairData.kt
+++ pluto-plugins/base/lib/src/main/java/com/pluto/utilities/views/keyvalue/KeyValuePairData.kt
@@ -1,6 +1,10 @@
 package com.pluto.utilities.views.keyvalue
 
+import android.os.Parcelable
+import android.text.InputType
 import androidx.annotation.Keep
+import com.pluto.utilities.list.ListItem
+import kotlinx.parcelize.Parcelize
 
 @Keep
 data class KeyValuePairData(
@@ -8,6 +12,31 @@ data class KeyValuePairData(
     val value: CharSequence?,
     val iconStart: Int? = null,
     val showClickIndicator: Boolean = false,
-    val isClickable: Boolean = false,
     val onClick: (() -> Unit)? = null
 )
+
+@Keep
+@Parcelize
+data class KeyValuePairEditRequest(
+    val key: String,
+    val value: String?,
+    val hint: String?,
+    val candidateOptions: List<String>? = null,
+    val allowFreeEdit: Boolean = true,
+    val inputType: KeyValuePairEditInputType
+) : ListItem(), Parcelable
+
+@Keep
+data class KeyValuePairEditResult(
+    val data: KeyValuePairEditRequest,
+    val result: String?
+)
+
+@Keep
+sealed class KeyValuePairEditInputType(val type: Int) : Parcelable {
+    @Parcelize object Integer : KeyValuePairEditInputType(InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED)
+
+    @Parcelize object Float : KeyValuePairEditInputType(InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED or InputType.TYPE_NUMBER_FLAG_DECIMAL)
+
+    @Parcelize object String : KeyValuePairEditInputType(InputType.TYPE_CLASS_TEXT)
+}
---
+++ pluto-plugins/base/lib/src/main/java/com/pluto/utilities/views/keyvalue/edit/KeyValuePairEditor.kt
@@ -0,0 +1,34 @@
+package com.pluto.utilities.views.keyvalue.edit
+
+import android.text.InputType
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+
+fun Fragment.lazyKeyValuePairEditor(): Lazy<KeyValuePairEditViewModel> = activityViewModels()
+
+fun ComponentActivity.lazyKeyValuePairEditor(): Lazy<KeyValuePairEditViewModel> = viewModels()
+
+
+sealed class ValueType(keyboardInputType: Int? = null, selectionOptions: List<String>? = null) {
+    class Number(selectionOptions: List<String>? = null) : ValueType(
+        keyboardInputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED,
+        selectionOptions = selectionOptions
+    )
+
+    class Float(selectionOptions: List<String>? = null) : ValueType(
+        keyboardInputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED or InputType.TYPE_NUMBER_FLAG_DECIMAL,
+        selectionOptions = selectionOptions
+    )
+
+    class Text(selectionOptions: List<String>? = null) : ValueType(keyboardInputType = InputType.TYPE_CLASS_TEXT, selectionOptions = selectionOptions)
+    object Boolean : ValueType(
+        selectionOptions = listOf(
+            "true",
+            "false"
+        )
+    )
+
+    class Selection(selectionOptions: List<String>? = null) : ValueType(selectionOptions = selectionOptions)
+}
