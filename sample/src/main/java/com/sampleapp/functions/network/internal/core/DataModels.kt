6e49fa39bcc94f1120771e600c66b58a8b652d3d
srtvprateek <srtv.prateek@gmail.com>
2023-03-25 00:35:21 +0530

    fix(sample): moshi adapter fix for sample module

--- sample/src/main/java/com/sampleapp/functions/network/internal/core/DataModels.kt
+++ sample/src/main/java/com/sampleapp/functions/network/internal/core/DataModels.kt
@@ -1,11 +1,15 @@
 package com.sampleapp.functions.network.internal.core
 
+import com.squareup.moshi.JsonClass
+
 sealed class ResponseWrapper<out T> {
     data class Success<out T>(val body: T) : ResponseWrapper<T>()
     data class Failure(val error: ErrorResponse, val errorString: String? = null) :
         ResponseWrapper<Nothing>()
 }
 
+@JsonClass(generateAdapter = true)
 data class ErrorResponse(val reason: String?, val error: String)
 
+@JsonClass(generateAdapter = true)
 data class StatusResponse(val status: Boolean)
