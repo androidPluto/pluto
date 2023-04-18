4b75a5e96847c72eaf5c2a3e402bcaa4f11ff75c
srtvprateek <srtv.prateek@gmail.com>
2023-04-18 11:39:20 +0530

    documentation

--- pluto-plugins/plugins/network/lib/src/main/java/com/pluto/plugins/network/DataModel.kt
+++ pluto-plugins/plugins/network/lib/src/main/java/com/pluto/plugins/network/DataModel.kt
@@ -7,24 +7,44 @@ import com.pluto.plugins.network.internal.interceptor.logic.Status
 import com.pluto.plugins.network.internal.interceptor.logic.core.mapCode2Message
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
+/**
+ * Request class for Custom Network traces
+ *
+ * @param url request url
+ * @param method request method
+ * @param body request body
+ * @param headers request header map
+ * @param sendTimeMillis request send timestamp
+ * @param isGzipped is request gzipped
+ */
 data class CustomRequest(
     val url: String,
-    val method: String,
+    val method: RequestMethod,
     val body: CustomBody?,
     val headers: Map<String, String?>,
-    val timestamp: Long,
+    val sendTimeMillis: Long,
     val isGzipped: Boolean
 ) {
     internal fun toRequestData(): RequestData = RequestData(
         url = url.toHttpUrl(),
-        method = method,
+        method = method.name,
         body = body?.toProcessBody(),
         headers = headers,
-        timestamp = timestamp,
+        timestamp = sendTimeMillis,
         isGzipped = isGzipped
     )
 }
 
+/**
+ * Response class for Custom Network traces
+ *
+ * @param statusCode response HTTP status code
+ * @param body response body
+ * @param headers response header map
+ * @param sendTimeMillis request send timestamp
+ * @param receiveTimeMillis response receive timestamp
+ * @param isGzipped is response gzipped
+ */
 data class CustomResponse(
     val statusCode: Int,
     val body: CustomBody?,
@@ -49,13 +69,46 @@ data class CustomResponse(
 
 data class CustomBody(
     val body: CharSequence,
-    val mediaType: String,
-    val mediaSubtype: String
+    val mediaType: BodyMediaType,
+    val mediaSubtype: BodyMediaSubType
 ) {
     internal fun toProcessBody(): ProcessedBody = ProcessedBody(
         isValid = true,
         body = body,
-        mediaType = mediaType,
-        mediaSubtype = mediaSubtype
+        mediaType = mediaType.name.lowercase(),
+        mediaSubtype = mediaSubtype.name.lowercase()
     )
+}
+
+enum class RequestMethod {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE
+}
+
+/**
+ * Custom representation of high-level media type, such as "text", "image", "audio", "video", or "application".
+ */
+enum class BodyMediaType {
+    APPLICATION,
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO
+}
+
+/**
+ * Custom representation of specific media subtype, such as "plain" or "png", "mpeg", "mp4" or "xml"
+ */
+enum class BodyMediaSubType {
+    JSON,
+    XML,
+    HTML,
+    X_WWW_FORM_URLENCODED,
+    PLAIN,
+    PNG,
+    MPEG,
+    MP4
 }
--- sample/src/main/java/com/sampleapp/functions/network/DemoNetworkFragment.kt
+++ sample/src/main/java/com/sampleapp/functions/network/DemoNetworkFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.pluto.plugins.network.BodyMediaSubType
+import com.pluto.plugins.network.BodyMediaType
 import com.pluto.plugins.network.CustomBody
 import com.pluto.plugins.network.CustomRequest
 import com.pluto.plugins.network.CustomResponse
 import com.pluto.plugins.network.PlutoNetwork
+import com.pluto.plugins.network.RequestMethod
 import com.sampleapp.R
 import com.sampleapp.databinding.FragmentDemoNetworkBinding
 import com.sampleapp.functions.network.internal.NetworkViewModel
@@ -43,22 +46,22 @@ class DemoNetworkFragment : Fragment(R.layout.fragment_demo_network) {
             PlutoNetwork.logCustomTrace(
                 request = CustomRequest(
                     url = "https://google.com",
-                    method = "GET",
+                    method = RequestMethod.GET,
                     body = CustomBody(
                         body = "hello request",
-                        mediaType = "text",
-                        mediaSubtype = "text"
+                        mediaType = BodyMediaType.APPLICATION,
+                        mediaSubtype = BodyMediaSubType.PLAIN
                     ),
                     headers = emptyMap(),
-                    timestamp = System.currentTimeMillis(),
+                    sendTimeMillis = System.currentTimeMillis(),
                     isGzipped = false
                 ),
                 response = CustomResponse(
                     statusCode = 200,
                     body = CustomBody(
                         body = "hello response",
-                        mediaType = "text",
-                        mediaSubtype = "text"
+                        mediaType = BodyMediaType.TEXT,
+                        mediaSubtype = BodyMediaSubType.PLAIN
                     ),
                     headers = hashMapOf(
                         "custom_header" to "custom header value"
