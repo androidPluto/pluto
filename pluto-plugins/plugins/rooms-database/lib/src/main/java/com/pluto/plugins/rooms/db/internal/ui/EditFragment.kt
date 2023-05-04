2475bba876df6e9691edac4dcbd435082719e79f
srtvprateek <srtv.prateek@gmail.com>
2023-05-04 23:29:50 +0530

    crash fix

--- pluto-plugins/plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/ui/EditFragment.kt
+++ pluto-plugins/plugins/rooms-database/lib/src/main/java/com/pluto/plugins/rooms/db/internal/ui/EditFragment.kt
@@ -45,6 +45,7 @@ internal class EditFragment : BottomSheetDialogFragment() {
     private val viewModel: ContentViewModel by activityViewModels()
     private val uiViewModel: UIViewModel by viewModels()
     private val sharer: ContentShareViewModel by lazyContentSharer()
+    private lateinit var deviceInfo: Device
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.pluto_rooms___fragment_data_editor, container, false)
@@ -57,6 +58,11 @@ internal class EditFragment : BottomSheetDialogFragment() {
     private val etList = mutableListOf<DataEditWidget>()
     private val fieldValues
         get() = etList.map { it.get() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        deviceInfo = Device(requireContext())
+    }
 
     override fun onStart() {
         super.onStart()
@@ -71,7 +77,7 @@ internal class EditFragment : BottomSheetDialogFragment() {
             val dialog = it as BottomSheetDialog
             val bottomSheet = dialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
             bottomSheet?.let {
-                dialog.behavior.peekHeight = Device(requireContext()).screen.heightPx
+                dialog.behavior.peekHeight = deviceInfo.screen.heightPx
                 dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
             }
         }
