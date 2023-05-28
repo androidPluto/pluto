b929ea11f313e6613e6d69fe3ba64eff0b2dd688
Rishabh Singh Bisht <rishabhsinghbisht@gmail.com>
2023-05-28 13:24:35 +0530

    fix(network): int being shown as flow. fixed

--- pluto-plugins/plugins/network/lib/src/main/java/com/pluto/plugins/network/internal/interceptor/logic/transformers/JsonTransformer.kt
+++ pluto-plugins/plugins/network/lib/src/main/java/com/pluto/plugins/network/internal/interceptor/logic/transformers/JsonTransformer.kt
@@ -3,29 +3,18 @@ package com.pluto.plugins.network.internal.interceptor.logic.transformers
 import com.pluto.plugins.network.internal.interceptor.logic.LOGTAG
 import com.pluto.utilities.DebugLog
 import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 
 internal class JsonTransformer : BaseTransformer {
 
-    private val moshi: Moshi = Moshi.Builder().build()
-    private val moshiObjectAdapter: JsonAdapter<Map<String, Any?>?> =
-        moshi.adapter(Types.newParameterizedType(Map::class.java, String::class.java, Any::class.java))
-    private val moshiArrayAdapter: JsonAdapter<List<Any?>?> = moshi.adapter(Types.newParameterizedType(List::class.java, Any::class.java))
+    private val moshi: Moshi = Moshi.Builder().add(AnyJsonAdapter()).build()
+    private val moshiAnyAdapter: JsonAdapter<Any> = moshi.adapter(Any::class.java)
 
     @SuppressWarnings("TooGenericExceptionCaught")
     override fun beautify(plain: CharSequence): CharSequence {
-        // todo fix this, find a better way to detect array/object
         return try {
-            try {
-                val jsonObject = moshiObjectAdapter.fromJson(plain.toString())
-                moshiObjectAdapter.indent(INDENTATION_TEXT).toJson(jsonObject)
-            } catch (e: JsonDataException) {
-                DebugLog.e(LOGTAG, "json object conversion failed, trying array conversion")
-                val jsonArray = moshiArrayAdapter.fromJson(plain.toString())
-                moshiArrayAdapter.indent(INDENTATION_TEXT).toJson(jsonArray)
-            }
+                val jsonObject = moshiAnyAdapter.fromJson(plain.toString())
+                moshiAnyAdapter.indent(INDENTATION_TEXT).toJson(jsonObject)
         } catch (e: Exception) {
             DebugLog.e(LOGTAG, "json parsing failed", e)
             plain
