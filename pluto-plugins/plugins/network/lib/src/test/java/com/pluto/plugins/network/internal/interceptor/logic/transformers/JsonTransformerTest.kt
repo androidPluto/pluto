b32b5bdd48d8a3b911ed805e9baeeb490bc92b50
Rishabh Singh Bisht <rishabhsinghbisht@gmail.com>
2023-05-28 13:37:48 +0530

    fix detekt ktlint

--- pluto-plugins/plugins/network/lib/src/test/java/com/pluto/plugins/network/internal/interceptor/logic/transformers/JsonTransformerTest.kt
+++ pluto-plugins/plugins/network/lib/src/test/java/com/pluto/plugins/network/internal/interceptor/logic/transformers/JsonTransformerTest.kt
@@ -5,20 +5,20 @@ import org.junit.Test
 class JsonTransformerTest {
 
     @Test
-    fun test_int_parsing(){
+    fun test_int_parsing() {
         val actualOutPut =
             JsonTransformer().beautify("""{"int":10}""".trimIndent())
 
         val expectedOutput = """{
                         |		"int": 10
                         |}""".trimMargin()
-        assert(actualOutPut == expectedOutput){
+        assert(actualOutPut == expectedOutput) {
             "\nExpected output is \n$expectedOutput\nbut actual output is \n$actualOutPut"
         }
     }
 
     @Test
-    fun test_array_parsing(){
+    fun test_array_parsing() {
         val actualOutPut =
             JsonTransformer().beautify("""[20,10]""".trimIndent())
 
@@ -26,7 +26,7 @@ class JsonTransformerTest {
                         |		20,
                         |		10
                         |]""".trimMargin()
-        assert(actualOutPut == expectedOutput){
+        assert(actualOutPut == expectedOutput) {
             "\nExpected output is \n$expectedOutput\nbut actual output is \n$actualOutPut"
         }
     }
