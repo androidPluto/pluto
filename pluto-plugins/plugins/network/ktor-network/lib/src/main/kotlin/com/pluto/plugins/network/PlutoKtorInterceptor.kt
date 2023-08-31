2c1dd598fcd44c46e0bacdc4b1e11ea1ccd0662e
srtvprateek <srtv.prateek@gmail.com>
2023-08-31 18:15:41 +0530

    mock setting in ktor interceptor

--- pluto-plugins/plugins/network/ktor-network/lib/src/main/kotlin/com/pluto/plugins/network/PlutoKtorInterceptor.kt
+++ pluto-plugins/plugins/network/ktor-network/lib/src/main/kotlin/com/pluto/plugins/network/PlutoKtorInterceptor.kt
@@ -5,6 +5,7 @@ import com.pluto.plugins.network.internal.KtorResponseConverter.convert
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.plugin
+import io.ktor.client.request.url
 import io.ktor.utils.io.errors.IOException
 
 fun HttpClient.addPlutoKtorPlugin() {
@@ -12,6 +13,7 @@ fun HttpClient.addPlutoKtorPlugin() {
         val request = requestUnBuilt.build()
         val networkRecorder = NetworkRecorder(request.convert())
         val callResult = try {
+            requestUnBuilt.url(networkRecorder.requestUrlWithMockInfo)
             execute(requestUnBuilt)
         } catch (e: IOException) {
             networkRecorder.onError(e)
