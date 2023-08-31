6350179512a6c5a8e2e23c9c561997113d08841a
srtvprateek <srtv.prateek@gmail.com>
2023-08-31 23:47:51 +0530

    publish config fixes

---
+++ pluto-plugins/plugins/network/ktor-network/lib-no-op/src/main/java/com/pluto/plugins/network/PlutoKtorInterceptor.kt
@@ -0,0 +1,7 @@
+package com.pluto.plugins.network
+
+import io.ktor.client.HttpClient
+
+@SuppressWarnings("EmptyFunctionBlock")
+fun HttpClient.addPlutoKtorPlugin() {
+}
--- pluto-plugins/plugins/network/okhttp-network/lib-no-op/src/main/java/com/pluto/plugins/network/PlutoInterceptor.kt
+++ pluto-plugins/plugins/network/okhttp-network/lib-no-op/src/main/java/com/pluto/plugins/network/PlutoInterceptor.kt
@@ -1,8 +1,10 @@
 package com.pluto.plugins.network
 
+import androidx.annotation.Keep
 import okhttp3.Interceptor
 import okhttp3.Response
 
+@Keep
 class PlutoInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
