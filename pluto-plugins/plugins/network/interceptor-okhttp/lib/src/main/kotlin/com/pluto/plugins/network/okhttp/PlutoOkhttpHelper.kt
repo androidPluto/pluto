8192839dc1e0647a27a38afe50dcbd8a96e6618b
srtvprateek <srtv.prateek@gmail.com>
2023-10-06 14:15:31 +0530

    fix(network): setting return type of addPlutoOkhttpInterceptor

--- pluto-plugins/plugins/network/interceptor-okhttp/lib/src/main/kotlin/com/pluto/plugins/network/okhttp/PlutoOkhttpHelper.kt
+++ pluto-plugins/plugins/network/interceptor-okhttp/lib/src/main/kotlin/com/pluto/plugins/network/okhttp/PlutoOkhttpHelper.kt
@@ -3,8 +3,9 @@ package com.pluto.plugins.network.okhttp
 import okhttp3.OkHttpClient
 import javax.net.SocketFactory
 
-fun OkHttpClient.Builder.addPlutoOkhttpInterceptor() {
+fun OkHttpClient.Builder.addPlutoOkhttpInterceptor(): OkHttpClient.Builder {
     // todo add okhttp settings block here
     socketFactory(SocketFactory.getDefault())
     addInterceptor(PlutoInterceptor())
+    return this
 }
