4e05af68c597be5af88d688ba686efda21d2300f
Aditya Kurkure <56596662+epicadk@users.noreply.github.com>
2023-11-28 11:45:33 +0530

    fix: possible content convert exception (#294)

--- pluto-plugins/plugins/network/interceptor-ktor/lib/src/main/kotlin/com/pluto/plugins/network/ktor/PlutoKtorHelper.kt
+++ pluto-plugins/plugins/network/interceptor-ktor/lib/src/main/kotlin/com/pluto/plugins/network/ktor/PlutoKtorHelper.kt
@@ -4,13 +4,18 @@ import com.pluto.plugins.network.intercept.NetworkInterceptor
 import com.pluto.plugins.network.ktor.internal.KtorRequestConverter.convert
 import com.pluto.plugins.network.ktor.internal.KtorResponseConverter.convert
 import io.ktor.client.HttpClient
+import io.ktor.client.call.save
+import io.ktor.client.plugins.HttpClientPlugin
 import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.plugin
 import io.ktor.client.request.url
+import io.ktor.util.AttributeKey
 import io.ktor.utils.io.errors.IOException
 
+private val saveAttributeKey = AttributeKey<Unit>("ResponseBodySaved")
+
+@Deprecated("install the PlutoKtorInterceptor plugin instead")
 fun HttpClient.addPlutoKtorInterceptor() {
-    // todo add ktor settings block here
     plugin(HttpSend).intercept { requestUnBuilt ->
         val request = requestUnBuilt.build()
         val networkInterceptor = NetworkInterceptor.intercept(request.convert(), NetworkInterceptor.Option(NAME))
@@ -21,10 +26,35 @@ fun HttpClient.addPlutoKtorInterceptor() {
             networkInterceptor.onError(e)
             throw e
         }
+        val res = if (callResult.attributes.contains(saveAttributeKey)) {
+            callResult
+        } else {
+            val newCall = callResult.save()
+            newCall.attributes.put(saveAttributeKey, Unit)
+            newCall
+        }
         networkInterceptor.onResponse(callResult.response.convert())
-        callResult
+        res
     }
 }
+
+class PlutoKtorInterceptor {
+    companion object : HttpClientPlugin<Unit, PlutoKtorInterceptor> {
+
+        override val key: AttributeKey<PlutoKtorInterceptor>
+            get() = AttributeKey("PlutoKtorInterceptor")
+
+        override fun prepare(block: Unit.() -> Unit): PlutoKtorInterceptor {
+            return PlutoKtorInterceptor()
+        }
+
+        override fun install(plugin: PlutoKtorInterceptor, scope: HttpClient) {
+            scope.addPlutoKtorInterceptor()
+        }
+
+    }
+}
+
 
 private const val NAME = "Ktor"
 
