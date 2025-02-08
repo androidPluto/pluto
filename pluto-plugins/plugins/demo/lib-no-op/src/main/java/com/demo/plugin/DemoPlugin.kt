f9c1806be42c8fa1ae8b84be390634b29655475d
Prateek <srtv.prateek@gmail.com>
2025-02-08 23:59:11 +0530

    chore: publish & versioning cleanup (#334)
    * deprecating nexus publishing
    
    * gradle cleanup
    
    * module publish cleanup
    
    * name refactor
    
    * publish code refactor

--- pluto-plugins/plugins/demo/lib-no-op/src/main/java/com/demo/plugin/DemoPlugin.kt
+++
@@ -1,8 +0,0 @@
-package com.demo.plugin
-
-@SuppressWarnings("UnusedPrivateMember")
-class DemoPlugin @JvmOverloads constructor(identifier: String = ID) {
-    companion object {
-        const val ID = "demo"
-    }
-}
