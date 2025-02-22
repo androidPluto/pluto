464d33eab23ecd1bacffc7f0c621ba1586366fcc
srtvprateek <srtv.prateek@gmail.com>
2025-02-22 17:58:48 +0530

    datastore & exceptions - groovy to kotlin

---
+++ pluto-plugins/plugins/exceptions/lib-no-op/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+val version = Versioning.loadVersioningData()
+val verCode = version["code"] as Int
+val verPublish = version["publish"] as String
+val verGitSHA = version["gitSha"] as String
+
+extra["PUBLISH_GROUP_ID"] = "com.plutolib.plugins"
+extra["PUBLISH_ARTIFACT_ID"] = "exceptions-no-op"
+extra["PUBLISH_VERSION"] = verPublish
+
+android {
+    namespace = "com.pluto.plugins.exceptions"
+
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    buildToolsVersion = libs.versions.buildTools.get()
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        buildConfigField("String", "VERSION_NAME", "\"$verPublish\"")
+        buildConfigField("long", "VERSION_CODE", "$verCode")
+        buildConfigField("String", "GIT_SHA", "\"$verGitSHA\"")
+    }
+
+    buildTypes {
+        getByName("release") {
+//            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.java.get()
+    }
+
+    lint {
+        abortOnError = false
+        targetSdk = libs.versions.targetSdk.get().toInt()
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+}
